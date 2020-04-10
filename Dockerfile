2c9ca74c525394e6ccac3153e08ff00e0a7ccf39
Ze Quan <zequanong@gmail.com>
2020-04-10 13:03:36 +0800

    Make it pass again!

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.7.3-stretch
-MAKE IT FAIL TOO!
+
 ## Step 1:
 # Create a working directory
 WORKDIR /app
--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 def hello():
     return "Hello World from Ze Quan. This is Blue."
 
-MAKEITFAIL!
 
 if __name__ == "__main__":
     # Only for debugging while developing
