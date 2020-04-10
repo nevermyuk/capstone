14a2c171495502ee296da9b5ed530a91abf75a52
Ze Quan <zequanong@gmail.com>
2020-04-10 12:59:26 +0800

    make it fail

--- app/app.py
+++ app/app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 def hello():
     return "Hello World from Ze Quan. This is Blue."
 
+MAKEITFAIL!
+
 if __name__ == "__main__":
     # Only for debugging while developing
     app.run(host='0.0.0.0', debug=True, port=80)
